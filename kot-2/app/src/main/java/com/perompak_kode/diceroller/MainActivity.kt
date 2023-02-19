5bf890599a3af1ace1f8774ff6a45aad33bb8d70
jimbonlemu <iqbaltech29@gmail.com>
2023-02-19 17:54:08 +0700

    adding kot-1, kot2, and birthday package

---
+++ kot-2/app/src/main/java/com/perompak_kode/diceroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.perompak_kode.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
