b6e2a3996a02664a88ffbe3bfdc503d452ceacac
jimbonlemu <iqbaltech29@gmail.com>
2023-02-20 21:22:11 +0700

    adding assets dice_images, update dice rolled based on image and toast showed the value of dice

--- kot-2/app/src/main/java/com/perompak_kode/diceroller/MainActivity.kt
+++ kot-2/app/src/main/java/com/perompak_kode/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.perompak_kode.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,27 +12,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton : Button =   findViewById(R.id.button)
             rollButton.setOnClickListener {
-                val toast = Toast.makeText(this,"Dice Rolled", Toast.LENGTH_SHORT)
-                toast.show()
-                val resultTextView :TextView = findViewById(R.id.textView)
-                val resultTextView2 :TextView = findViewById(R.id.textView2
-                )
 
-                resultTextView.text = "6"
-                resultTextView2.text = "6"
                 rollDice()
             }
     }
-
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        val resultTextView: TextView = findViewById(R.id.textView)
-        val resultTextView2: TextView = findViewById(R.id.textView2)
+        // Find the ImageView in the layout
+        val diceImage : ImageView = findViewById(R.id.imageView)
 
-        resultTextView.text = diceRoll.toString()
-        resultTextView2.text = diceRoll.toString()
+        // Determine which drawable resource ID to use based on the dice roll
+       val drawableResource =  when (diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+       }
+        val toast = Toast.makeText(this,"You rolled a ${diceRoll} ", Toast.LENGTH_SHORT)
+        toast.show()
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
+
     }
 }
 
