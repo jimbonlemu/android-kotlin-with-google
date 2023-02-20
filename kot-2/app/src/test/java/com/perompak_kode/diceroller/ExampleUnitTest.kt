74d205b741b2278e5240f463b2ce91647f51bb02
jimbonlemu <iqbaltech29@gmail.com>
2023-02-21 01:23:08 +0700

    ExampleUnitTest 3 Test Passed

--- kot-2/app/src/test/java/com/perompak_kode/diceroller/ExampleUnitTest.kt
+++ kot-2/app/src/test/java/com/perompak_kode/diceroller/ExampleUnitTest.kt
@@ -1,9 +1,8 @@
 package com.perompak_kode.diceroller
 
+
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -11,7 +10,24 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generate_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
+
+        }
+    @Test
+    fun generate_number2() {
+        val dice = Dice(4)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
+
+    }
+    @Test
+    fun generate_number3() {
+        val dice = Dice(5)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
+
     }
 }
